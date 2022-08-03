bd86f57f7c978a0cac48b684273303c5d8bfa1dc
jahnvigagnwar <jahnvigagnwar@gmail.com>
2022-08-03 10:05:04 +0000

    week 2

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ["127.0.0.1","jahnvi.eu-gb.mybluemix.net","localhost"]
+ALLOWED_HOSTS =  ["127.0.0.1","jahnvi.eu-gb.mybluemix.net","localhost", "https://jahnvigangwa-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"]
 
 
 # Application definition
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
+
